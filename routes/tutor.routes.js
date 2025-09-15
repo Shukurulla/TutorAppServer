56030c543281253e2c4701211ee2a1b48899ba51
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-09-15 11:55:53 +0500

    start

--- routes/tutor.routes.js
+++ routes/tutor.routes.js
@@ -836,7 +836,7 @@ router.get(
       }
 
       // 5️⃣ StudentId larni olish (ObjectId tarzida)
-      const studentIds = findStudents.map((s) => s._id);
+      const studentIds = findStudents.map((s) => s._id.toString());
 
       // 6️⃣ Aggregation bilan oxirgi appartmentlarni olish
       const studentAppartments = await AppartmentModel.aggregate([
@@ -844,7 +844,7 @@ router.get(
           $match: {
             studentId: { $in: studentIds },
             typeAppartment: "tenant",
-            permission: findActivePermission._id,
+            permission: findActivePermission._id.toString(),
           },
         },
         {
