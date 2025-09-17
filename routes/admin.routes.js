b3abd6f1450c5c5591420582da0de8f7976992bb
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-09-17 15:44:17 +0500

    start

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -54,6 +54,15 @@ router.get("/admin/faculty-admins/search", authMiddleware, async (req, res) => {
   }
 });
 
+router.post("/admin/sign", async (req, res) => {
+  try {
+    const admin = await adminModel.create(req.body);
+    res.status(200).json({ status: "success", data: admin });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+});
+
 // Faculty admin uchun tutorlarni search qilish
 router.get("/faculty-admin/tutors/search", authMiddleware, async (req, res) => {
   try {
